db9e1ab38d5b61dffccdcf21017f6c34c1a12e25
AnujRuhela7 <ruhela7777@gmail.com>
2023-04-28 11:28:46 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const { connection } = require('./db.js');
-const cookieParser = require('cookie-parser');
-
+const express = require("express");
+const { connection } = require("./db.js");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // Defining the app
 const app = express();
 
-
 // Middlewares
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({extended: true, limit: '50mb'}));
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
 // Requiring the routers
-const authRouter = require('./routes/authRoute');
-const contactusRouter = require('./routes/contactusRoute');
-const forgotPasswordRouter = require('./routes/forgotPasswordRoute');
-const verifyOTPRouter = require('./routes/verifyOTPRoute');
-const changePasswordRouter = require('./routes/changePasswordRoute');
-const propertyRouter = require('./routes/propertyRoute');
-const verifyEmailRouter = require('./routes/verifyEmailRoute');
-const cartRouter = require('./routes/cartRoute');
-const bookAppointementRouter = require('./routes/bookAppointmentRoute');
-const paymentRouter = require('./routes/paymentRoute');
+const authRouter = require("./routes/authRoute");
+const contactusRouter = require("./routes/contactusRoute");
+const forgotPasswordRouter = require("./routes/forgotPasswordRoute");
+const verifyOTPRouter = require("./routes/verifyOTPRoute");
+const changePasswordRouter = require("./routes/changePasswordRoute");
+const propertyRouter = require("./routes/propertyRoute");
+const verifyEmailRouter = require("./routes/verifyEmailRoute");
+const cartRouter = require("./routes/cartRoute");
+const bookAppointementRouter = require("./routes/bookAppointmentRoute");
+const paymentRouter = require("./routes/paymentRoute");
 
 // Middlewares for setting the routers
-app.use('/', authRouter);
-app.use('/', contactusRouter);
-app.use('/', forgotPasswordRouter);
-app.use('/', verifyOTPRouter);
-app.use('/', changePasswordRouter);
-app.use('/', propertyRouter);
-app.use('/', verifyEmailRouter);
-app.use('/', cartRouter);
-app.use('/', bookAppointementRouter);
-app.use('/', paymentRouter);
+app.use("/", authRouter);
+app.use("/", contactusRouter);
+app.use("/", forgotPasswordRouter);
+app.use("/", verifyOTPRouter);
+app.use("/", changePasswordRouter);
+app.use("/", propertyRouter);
+app.use("/", verifyEmailRouter);
+app.use("/", cartRouter);
+app.use("/", bookAppointementRouter);
+app.use("/", paymentRouter);
 
 // Getting the port from the environment variables
 const PORT = process.env.PORT;
 
 // Starting the server
 app.listen(PORT, () => {
-    console.log("Server is running");
+  console.log("Server is running");
 });
 
-app.get('/', (req, res) => {
-    res.send("Hello");
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
 
 // Establishing the connection with the database
